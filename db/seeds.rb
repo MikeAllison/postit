967f59ed5e3c4df5592b1eeb66d167cc108edc4a
Mike Allison <mikeallison79@gmail.com>
2015-03-17 09:15:16 -0400

    Updated Bootstrap to 3.3.4, refactored layout, created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+  User.create(username: "User #{i + 1}")
+end
+
+10.times do |i|
+  Category.create(name: "Category #{i + 1}")
+end
+
+50.times do |i|
+  description = "This is description #{i + 1}."
+  Post.create(url: "www.site#{i + 1}.com", title: "Title #{i + 1}", description: description, user_id: rand(1..10))
+end
+
+30.times do |i|
+  Comment.create(body: "This is comment #{i + 1}", user_id: rand(1..10), post_id: rand(1..50))
+end
+
+# Assign a category to every post
+50.times do |i|
+  PostCategory.create(post_id: i + 1, category_id: rand(1..5))
+end
+
+# Assign extra categories to a random posts
+25.times do |i|
+  PostCategory.create(post_id: rand(1..50), category_id: rand(6..10))
+end
