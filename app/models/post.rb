1214779f84ce6b15feb5e002c6e59f67a5c76e0f
Mike Allison <mikeallison79@gmail.com>
2015-03-27 16:49:35 -0400

    Updated Post.url validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,8 @@ class Post < ActiveRecord::Base
   validates_length_of :title, minimum: 5, message: "Title is too short (minimum is 5 characters)"
   validates_presence_of :url, message: "URL field can't be blank"
   validates_uniqueness_of :url, case_sensitive: false, message: 'This URL has already been posted'
-  validates_format_of :url, with: "/A\S\z/", message: 'URL field cannot contain spaces'
-  validates_format_of :url, with: URI::regexp(['http', 'https']), message: 'URL field must be a URL (ex. http://www.example.com)'
+  validates_format_of :url, with: /https?:\/\/[\S]+/, message: 'URL field must be a valid URL (ex. http://www.example.com)'
+  validates_format_of :url, without: /\s\b/, message: 'URL field cannot contain spaces'
+  #validates_format_of :url, with: /#{URI::regexp(['http', 'https'])}/, message: 'URL field must be a valid URL (ex. http://www.example.com)'
   validates_presence_of :description, message: "Description field can't be blank"
 end
