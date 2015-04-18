5fc83527b5c817faa145220cb9754cde00a2be4f
Mike Allison <mikeallison79@gmail.com>
2015-04-18 15:02:17 -0400

    Maybe fixed a bug but can't really tell if it existed

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,8 @@
 module CommentsHelper
+
+  # Appends 'on (post.title) to a comment under a user profile
   def post_link(comment)
     raw "on #{link_to(comment.post.title, comment.post)}"
   end
+
 end
