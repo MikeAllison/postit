02afe9d703ca754f3db56a107f1aec0ece952eff
Mike Allison <mikeallison79@gmail.com>
2015-04-20 13:55:33 -0400

    Started creating User.has_voted_on? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username, case_sensitive: false, message: "This username has already been taken"
   validates_presence_of :password, message: "Password can't be blank"
   validates_confirmation_of :password, message: "The passwords don't match"
+
+  def has_voted_on?(post)
+    if self.votes.where("voteable_id = ?", post).exists?
+      post.votes.where("user_id = ?", self)
+    else
+      false
+    end
+  end
 end
