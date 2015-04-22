aaea74b158655b3c17a2d9302b25b83a13e0d2a4
Mike Allison <mikeallison79@gmail.com>
2015-04-22 16:28:09 -0400

    Extracted Post and Comment voting features into Votingable module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,23 +1,10 @@
 class Comment < ActiveRecord::Base
+  include Votingable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post, counter_cache: :comments_count
   has_many :votes, as: :voteable
 
   validates_presence_of :body, message: "Comment cannot be blank"
 
-  def tallied_votes
-    total_votes = 0
-    upvotes = self.votes.where("vote = ?", true).count
-    downvotes = self.votes.where("vote = ?", false).count
-
-    total_votes = upvotes - downvotes
-  end
-
-  def has_same_vote_from?(user, vote)
-    return true if self.votes.where("user_id = ? and vote = ?", user, vote).exists?
-  end
-
-  def has_opposite_vote_from?(user, vote)
-    return true if self.votes.where("user_id = ? and vote != ?", user, vote).exists?
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  #require 'uri'
+  include Votingable
 
   before_validation :strip_url_whitespace
   before_validation :downcase_url
@@ -16,25 +16,8 @@ class Post < ActiveRecord::Base
   validates_uniqueness_of :url, case_sensitive: false, message: "This URL has already been posted"
   validates_format_of :url, with: /https?:\/\/[\S]+/, message: "URL field must be a valid URL (ex. http://www.example.com)"
   validates_format_of :url, without: /\s\b/, message: "URL field cannot contain spaces"
-  #validates_format_of :url, with: /#{URI::regexp(['http', 'https'])}/, message: 'URL field must be a valid URL (ex. http://www.example.com)'
   validates_presence_of :description, message: "Description field can't be blank"
   validates_presence_of :categories, message: "Please select at least one category"
-
-  def tallied_votes
-    total_votes = 0
-    upvotes = self.votes.where("vote = ?", true).count
-    downvotes = self.votes.where("vote = ?", false).count
-
-    total_votes = upvotes - downvotes
-  end
-
-  def has_same_vote_from?(user, vote)
-    return true if self.votes.where("user_id = ? and vote = ?", user, vote).exists?
-  end
-
-  def has_opposite_vote_from?(user, vote)
-    return true if self.votes.where("user_id = ? and vote != ?", user, vote).exists?
-  end
 
   private
     def strip_url_whitespace
