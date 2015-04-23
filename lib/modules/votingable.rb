e0b39658e040c59ce20e5643d3b3035b6a47f335
Mike Allison <mikeallison79@gmail.com>
2015-04-23 09:04:31 -0400

    Added a comment to Comment.  Simplfied methods in Votingable.

--- lib/modules/votingable.rb
+++ lib/modules/votingable.rb
@@ -1,17 +1,16 @@
 module Votingable
   def tallied_votes
-    total_votes = 0
     upvotes = self.votes.where("vote = ?", true).count
     downvotes = self.votes.where("vote = ?", false).count
 
-    total_votes = upvotes - downvotes
+    upvotes - downvotes || 0
   end
 
   def has_same_vote_from?(user, vote)
-    return true if self.votes.where("user_id = ? and vote = ?", user, vote).exists?
+    self.votes.where("user_id = ? and vote = ?", user, vote).exists?
   end
 
   def has_opposite_vote_from?(user, vote)
-    return true if self.votes.where("user_id = ? and vote != ?", user, vote).exists?
+    self.votes.where("user_id = ? and vote = ?", user, !!vote).exists?
   end
 end
