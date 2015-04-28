838ae222dde887521f47577669775f6e1c6768d4
Mike Allison <mikeallison79@gmail.com>
2015-04-28 14:07:33 -0400

    Cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PostitTemplate::Application.routes.draw do
+
   root to: 'posts#index'
 
   get 'register', to: 'users#new'
@@ -7,12 +8,13 @@ PostitTemplate::Application.routes.draw do
   post  'login',  to: 'sessions#create'
   get   'logout', to: 'sessions#destroy'
 
+  # Sets vote_post_path and vote_comment_path (could also use 'member')
+  post 'posts/:id/vote',    to: 'posts#vote',     as: 'vote_post'
+  post 'comments/:id/vote', to: 'comments#vote',  as: 'vote_comment'
+
   resources :posts, except: [:destroy] do
-    post :vote, on: :member
     resources :comments, only: [:create]
   end
-
-  post 'comments/:id/vote', to: 'comments#vote', as: 'vote_comment'
 
   resources :categories, except: [:destroy]
   resources :users, except: [:index, :destroy]
