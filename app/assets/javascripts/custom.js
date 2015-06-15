524a5ebe1a2e92fcbb59c66b0c0719a9011c7417
Mike Allison <mikeallison79@gmail.com>
2015-06-15 13:14:05 -0400

    Fixed adjustNavStyles function.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,5 @@
 // Toggles between Bootstrap Pills and the normal nav CSS styles/classes based on window size
-function adjustNavStyles(windowWidth) {
+function adjustNavStyles() {
   var windowWidth = $(window).width();
 
   // Remove both navbar classes on page load
@@ -7,7 +7,7 @@ function adjustNavStyles(windowWidth) {
 
   // Use pills for nav links on screens >= 750
   // Use Bootstrap responsive menu on screens < 750
-  // Needs to be set to 747 to fix a display issue 
+  // Needs to be set to 747 to fix a display issue
   if (windowWidth >= 747) {
     $("ul#navbar-links").addClass("nav-pills");
   } else {
