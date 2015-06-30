36b9164a1ac1c1e15a950a7bc65713aeec2a8064
Mike Allison <mikeallison79@gmail.com>
2015-06-30 16:13:52 -0400

    Updated comments

--- app/assets/javascripts/override_confirm.js
+++ app/assets/javascripts/override_confirm.js
@@ -1,6 +1,6 @@
 // Override the default confirm dialog box in Rails
 
-// If data-confirm doesn't have a vaule in link_to...return
+// If data-confirm doesn't have a value in link_to, return
 // If it does have a value, call showConfirmationDialog
 $.rails.allowAction = function(link) {
   if (link.data("confirm") == undefined) {
@@ -10,16 +10,15 @@ $.rails.allowAction = function(link) {
   return false;
 }
 
-// If confirm button is clicked:
-// 1. Set data-confirm to null
-// 2. Continue executing action
+// Set data-confirm to null
+// Continue executing action
 $.rails.confirmed = function(link) {
   link.data("confirm", null);
   link.trigger("click.rails");
 }
 
 // Display the confirmation with bootbox styling
-// If result === true, call confirmed
+// If 'OK' clicked (result === true), call confirmed
 $.rails.showConfirmationDialog = function(link) {
   var message = link.data("confirm");
   bootbox.confirm(message, function(result) {
