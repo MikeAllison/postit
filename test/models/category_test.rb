fdad669b75287529bec6242f2868eb3f3bb52805
Mike Allison <mikeallison79@gmail.com>
2016-08-31 09:34:05 -0400

    Added Comment unit tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -9,34 +9,34 @@ class CategoryTest < ActiveSupport::TestCase
   test 'cannot save a category without a name' do
     c = Category.create(name: '')
     assert_not c.persisted?
-    assert 'Name cannot be blank', c.errors.messages[:name]
+    assert_equal 'Name cannot be blank', c.errors.messages[:name].first
   end
 
   test 'duplicate categories will not be saved' do
     c = Category.create(name: 'News')
     c2 = Category.create(name: 'News')
     assert_not c2.persisted?
-    assert 'This category already exists', c2.errors.messages[:name]
+    assert_equal 'This category already exists', c2.errors.messages[:name].first
   end
 
   test 'cannot create a category with over 15 characters' do
     c = Category.create(name: 'superlongcategoryname')
     assert_not c.persisted?
-    assert 'Category name must be less than 15 characters', c.errors.messages[:name]
+    assert_equal 'Category name must be less than 15 characters', c.errors.messages[:name].first
   end
 
-  test 'sets unhidden_posts_count to 0 on initialization' do
+  test 'initialize_posts_count' do
     c = Category.new
     assert_equal 0, c.unhidden_posts_count
   end
 
-  test 'increase_unhidden_posts_count works' do
+  test 'increase_unhidden_posts_count' do
     c = Category.create(name: 'News')
     c.increase_unhidden_posts_count
     assert_equal 1, c.unhidden_posts_count
   end
 
-  test 'reduce_unhidden_posts_count works' do
+  test 'reduce_unhidden_posts_count' do
     c = Category.create(name: 'News')
     c.increase_unhidden_posts_count
     c.reduce_unhidden_posts_count
---
+++ test/models/comment_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+  test 'can add a valid comment' do
+    c = Comment.create(body: 'A valid comment')
+    assert c.persisted?
+  end
+
+  test 'cannot save a comment without a body' do
+    c = Comment.create(body: '')
+    assert_not c.persisted?
+    assert_equal 'Comment cannot be blank', c.errors.messages[:body].first
+  end
+
+  test 'initialize_tallied_votes' do
+    c = Comment.new
+    assert_equal 0, c.tallied_votes
+  end
+
+  test 'clear_flags' do
+    c = Comment.create(body: 'A valid comment')
+    2.times do
+      c.flags.create
+    end
+    c.clear_flags
+    assert_equal 0, c.flags.count
+    assert_equal 0, c.total_flags
+  end
+
+  test 'hide' do
+    p = Post.new(url: 'http://www.abc.com',
+                     title: 'Valid Title',
+                     description: 'A valid description')
+    p.categories << Category.create(name: 'News')
+    p.save
+    c = p.comments.create(body: 'A valid comment')
+    2.times do
+      c.flags.create
+      c.votes.create
+    end
+    c.hide
+    assert c.hidden
+    assert_equal 0, c.flags.count
+    assert_equal 0, c.votes.count
+    assert_equal -1, c.post.unhidden_comments_count
+  end
+end
